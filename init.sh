c42f499e05b8baf227ab202cc3374004158a4ad7
Darius Marian <marian.darius98@gmail.com>
2021-09-04 12:57:51 +0300

    Add script to init sub-repos. Enable tests for all repo-s by default

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/darius98/mcga-cli cli
+git clone https://github.com/darius98/mcga-matchers matchers
+git clone https://github.com/darius98/mcga-proc proc
+git clone https://github.com/darius98/mcga-test test
+git clone https://github.com/darius98/mcga-threading threading
+
+git submodule update --init
+cd test
+git submodule update --init
+cd ../threading
+git submodule update --init
