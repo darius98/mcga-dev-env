4e68eadd20084db403fcf9bcf5ce572bca6f6955
Darius Marian <marian.darius98@gmail.com>
2022-03-23 22:29:15 +0200

    Add the meta project

--- init.sh
+++ init.sh
@@ -2,12 +2,12 @@
 
 git clone https://github.com/darius98/mcga-cli cli
 git clone https://github.com/darius98/mcga-matchers matchers
+git clone https://github.com/darius98/mcga-meta meta
 git clone https://github.com/darius98/mcga-proc proc
 git clone https://github.com/darius98/mcga-test test
 git clone https://github.com/darius98/mcga-threading threading
 
 git submodule update --init
-cd test
+cd threading
 git submodule update --init
-cd ../threading
-git submodule update --init
+cd ..
